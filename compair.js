3c44409a99556926bb6338bb9bffca26ff34bffa
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-01-07 09:32:17 +0000

    compair with operator

---
+++ compair.js
@@ -0,0 +1,54 @@
+// console.log("2" <1);
+// console.log(2 >"1");
+
+// console.log("2" > 1);
+// console.log("02" >= 1);
+// console.log("2"===2);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+console.log("Ther are two types of dataType.");
+console.log("Primitive and Non primitive dataType. Given below");
+
+console.log("Primitive:- string, number, Boolean, null, undefined, symbol, BigInt");
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// console.log(isLoggedIn == outsideTemp);
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+// const bigNumber =  36748578458784n
+// console.log(typeof bigNumber);
+
+
+// console.log("Non-Primitive:- Array, Objects, Function");
+
+// const heros = ["Karna", "ram", "mohan", "raghuvar"];
+// let myObj = {
+//     name: "vishal",
+//     age:21
+// }
+
+// const myFunction = function(){
+//     console.log("this function is object function");
+// }
+
+
